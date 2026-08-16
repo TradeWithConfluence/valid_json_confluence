cdd87bd6cd9f4d47a389492984aa1db44cba3951
Brian Weiss <113956118+BrianWeiss1@users.noreply.github.com>
2026-08-16 15:22:25 -0400

    ruff format

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
+from .classes import CandleSize
 from .functions import (
     extract_risk_params,
     get_indicator_conditions_from_jsons,
     obtain_conditions_for_setup_indicators,
 )
 from .validate_json import validate_json_file
-from .classes import CandleSize
